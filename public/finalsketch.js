61b6706808693cc02b6b905631ee21a2b7267788
silviacarpanoni <71377279+silviacarpanoni@users.noreply.github.com>
2021-01-21 16:13:44 +0100

    update project 21.01

--- public/finalsketch.js
+++ public/finalsketch.js
@@ -1,8 +1,14 @@
 // Create a new connection using socket.io (imported in index.html)
 let socket = io();
 let myColor = "white";
-let buttonBack;
+let buttonSend;
 var backgroundSound;
+
+let stars = [];
+let lines = [];
+
+// var star;
+
 
 
 // define the function that will be called on a new newConnection
@@ -52,34 +58,44 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background(9, 14, 36);
 
+
+  // star = { //star element to define behavior of shooting star
+  //   starX : mouseX,
+  //   starY : mouseY,
+  //   size : random(0.5, 1)
+  // }
+
   backgroundSound.setVolume(0.10);
   backgroundSound.play();
   backgroundSound.loop();
 
   push();
-  buttonBack = createButton("SEND");
-  buttonBack.position(windowWidth/2-buttonBack.width/2, windowHeight*12/14-buttonBack.height/2);
-  buttonBack.mousePressed();
+  buttonSend = createButton("SEND");
+  buttonSend.position(windowWidth/2-buttonSend.width/2, windowHeight*12/14-buttonSend.height/2);
+  // buttonSend.mousePressed(anotherStar);
   pop();
 
+  for (let i = 0; i < width * 0.22; i++) {
+    stars.push(new Constellation());
+  }
 }
 
 // Callback function called when a new message comes from the server
 // Data parameters will contain the received data
-
-
-
 function otherMouse(data) {
   console.log("received:", data);
   noStroke();
-  fill("yellow");
-  ellipse(data.x, data.y, 20);
+  fill("red");
+  ellipse(data.x, data.y, 4);
+
 }
 
-function mouseDragged() {
+function mouseClicked() {
   console.log("sending: ", mouseX, mouseY);
   noStroke();
   fill(255);
+  ellipse(mouseX, mouseY, 4);
+
 
   // create an object containing the mouse position
   let message = {
@@ -90,9 +106,93 @@ function mouseDragged() {
   // tag it as "mouse" event
   socket.emit("mouse", message);
 
-  ellipse(mouseX, mouseY, 20);
 }
 
 function draw() {
+  for (let i = 0; i < stars.length; i++) {
+      stars[i].twinkle();
+      stars[i].display();
+    }
 
+//   push();
+//   ellipseMode(CENTER); //Draw stars in the canvas
+//   fill(255, 255, 255, 20);
+//   strokeWeight(0);
+//   pop();
+//
+//
+//   //Draw shooting star
+// push();
+// ellipseMode(CENTER);
+// fill(255, 255, 255, 20);
+// strokeWeight(0);
+// ellipse(star.starX, star.starY, star.size, star.size);
+// star.starX = star.starX+1; //Move shoouting star
+// star.starY = star.starY-1;
+// star.size = star.size + 0.02
+// pop();
+ //Decreases the size of shooting star
+  }
+//
+//   function anotherStar() {
+//   star = {
+//   starX : random(windowWidth),
+//   starY : random(windowHeight),
+//   size : random(0.5, 3)
+// }
+//   }
+
+
+  function mousePressed() {
+    for (let i = 0; i < stars.length; i++) {
+      stars[i].starClick(mouseX, mouseY);
+      stars[i].lineDraw();
+    }
+  }
+
+  class Constellation {
+  constructor() {
+    this.x = random(width);
+    this.y = random(height);
+    this.d = random(1, 3);
+    this.clicked = false;
+  }
+
+  display() {
+    noStroke();
+    if (this.clicked == true) {
+      ellipse(this.x, this.y, this.d + 4);
+    } else {
+      ellipse(this.x, this.y, this.d);
+    }
+  }
+
+  twinkle() {
+    this.r = random(110, 255);
+    this.g = random(207, 254);
+    this.b = random(204, 255);
+    fill(this.r, this.g, this.b);
+  }
+
+
+  starClick(mx, my) {
+    if (dist(mx, my, this.x, this.y) < this.d / 2 + 1) {
+      this.clicked = !this.clicked;
+      lines.push(this.x, this.y);
+    }
+  }
+
+  lineDraw() {
+    if (lines.length == 4) {
+      stroke(239, 255, 253, 95);
+      line(lines[0], lines[1], lines[2], lines[3]);
+      lines.splice(0, 2);
+    }
+
+  // let message = {
+  //   x: this.x,
+  //   y: this.y,
+  // };
+  //   socket.emit("mouse", message);
+  }
 }

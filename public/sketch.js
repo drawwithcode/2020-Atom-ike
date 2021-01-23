f41d7b82ff5fd7bbf55878a882300ac1aebd3298
chiaradavoli <chiaradavoli@users.noreply.github.com>
2021-01-23 16:44:12 +0100

    added logo + mobile check

--- public/sketch.js
+++ public/sketch.js
@@ -1,45 +1,57 @@
-
+//detect mobile browser
 function checkIfMobile() {
-    let check = false;
-    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-    return check;
+  let check = false;
+  (function(a) {
+    if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true;
+  })(navigator.userAgent || navigator.vendor || window.opera);
+  return check;
 };
 
-
-
-// costanti stelle random + stelle cadenti
+// costanti generali browser e mobile
 var fromX, fromY;
 var toX, toY;
 var step = 2.5;
-let myTitle = "De-siderium";
-let buttonSound;
 
+let myTitle = "De-siderium";
+let myText = "Sorry, you can't make a wish on this browser.\nPlease connect your mobile device to use this WebApp!";
+
+let buttonSound;
+let myImageLogo;
+
+var maxDiameter;
+var theta;
+
+//mobile check sketch
 if (checkIfMobile()) {
 
   function preload() {
-  buttonSound = loadSound("./assets/sounds/Sound_effect_1.mp3");
+    buttonSound = loadSound("./assets/sounds/Sound_effect_1.mp3");
+    myImageLogo = loadImage("./assets/images/logostar.png");
+
   }
 
   function setup() {
     createCanvas(windowWidth, windowHeight);
     background(9, 14, 36);
 
+    //logo animazione
+    maxDiameter = 10;
+    theta = 0;
 
     //enter button
     push();
     buttonEnter = createButton("ENTER THE GALAXY");
-    buttonEnter.position(windowWidth/2-buttonEnter.width/2, windowHeight*8/9-buttonEnter.height/2);
+    buttonEnter.position(windowWidth / 2 - buttonEnter.width / 2, windowHeight * 8 / 9 - buttonEnter.height / 2);
     buttonEnter.mousePressed(changeSketch);
     pop();
   }
 
 
-
   function draw() {
-    // background("#090e24");
+
     background(9, 14, 36, 1);
 
-    // stelle random + stelle cadenti
+    // stelle + stelle cadenti
     if (random() > 0.92) {
       stroke(255);
       ellipse(random(width), random(height), random(0.1, 1.5));
@@ -75,11 +87,28 @@ if (checkIfMobile()) {
     rect(windowWidth / 2, windowHeight / 2.1, 250, 50);
     pop();
 
+
+    //logo animazione
+    var diam = 80 + sin(theta) * maxDiameter;
+
+    push();
+    noStroke();
+    fill(9, 14, 36);
+    rectMode(CENTER);
+    ellipse(windowWidth / 2, windowHeight / 2.8, 150, 150);
+    pop();
+
+    //logo immagine
+    imageMode(CENTER);
+    image(myImageLogo, width / 2, height / 2.8, diam, diam);
+    theta += .01;
+
     // testo Titolo
     push();
-    fill("#dddddb");
-    noStroke();
-    textSize(40);
+    fill(255, 255, 255);
+    stroke(9, 14, 36);
+    strokeWeight(2);
+    textSize(35);
     textAlign(CENTER);
     textFont("Prata");
     text(myTitle, windowWidth / 2, windowHeight / 2);
@@ -89,136 +118,128 @@ if (checkIfMobile()) {
     noStroke();
     fill(9, 14, 36);
     rectMode(CENTER);
-    rect(windowWidth/2, windowHeight*8/9, 180, 40);
+    rect(windowWidth / 2, windowHeight * 8 / 9, 180, 40);
     pop();
-
   }
+
 
   function changeSketch() {
     window.open('concept.html', '_self');
   }
-
-
-  //   function playSound() {
-  //
-  //   // If the user clicks on the button, play the sound!
-  // if(buttonSound.isPlaying() == false){
-  //
-  // buttonSound.play();
-  // }   window.open('concept.html', '_self');
-  //   }
-
 
   function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
   }
 
 
-}
+
+  //__________________________
 
 
-else {
+
+} else {
 
 
-  //codice da vedere su browser
+  //sketch browser
+  function preload() {
+
+    myImageLogo = loadImage("./assets/images/logostar.png");
+
+  }
+
+  function setup() {
+
+    createCanvas(windowWidth, windowHeight);
+    background(9, 14, 36);
+
+    //logo animazione
+    maxDiameter = 20;
+    theta = 0;
+  }
+
+  function draw() {
+
+    background(9, 14, 36, 1);
+
+    // stelle + stelle cadenti
+    if (random() > 0.92) {
+      stroke(255);
+      ellipse(random(width), random(height), random(0.1, 1.5));
+    }
+
+    if (random() > 0.95 && step >= 2.5) {
+      fromX = random(width);
+      fromY = random(height / 2);
+      toX = random(fromX + 10, -width);
+      toY = random(fromY + 10, -height / 2);
+      step = 0;
+    }
+
+    if (step < 2.5) {
+      let nextStep = step + 0.02;
+      strokeWeight(3);
+      stroke(9, 14, 36, 100);
+      line(fromY, fromX, toY, toX);
+      strokeWeight(0.7);
+
+      if (step < 1) {
+        stroke(255, (1 - step) * 200);
+        line(lerp(fromY, toY, step), lerp(fromX, toX, step),
+          lerp(fromY, toY, nextStep), lerp(fromX, toX, nextStep));
+      }
+      step = nextStep;
+    }
+
+    push();
+    noStroke();
+    fill(9, 14, 36);
+    rectMode(CENTER);
+    rect(windowWidth / 2, windowHeight / 1.45, 500, 130);
+    pop();
+
+    // testo Titolo
+    push();
+    fill("#dddddb");
+    noStroke();
+    textSize(40);
+    textAlign(CENTER);
+    textFont("Prata");
+    text(myTitle, windowWidth / 2, windowHeight / 1.5);
+    pop();
+
+    // testo browser
+    push();
+    fill("#dddddb");
+    stroke(9, 14, 36);
+    strokeWeight(1.5);
+    textSize(15);
+    textAlign(CENTER);
+    textFont("Roboto Mono");
+    text(myText, windowWidth / 2, windowHeight / 1.4);
+    pop();
+
+
+    //logo animazione
+    var diam = 120 + sin(theta) * maxDiameter;
+
+    push();
+    noStroke();
+    fill(9, 14, 36);
+    rectMode(CENTER);
+    ellipse(windowWidth / 2, windowHeight / 2, 150, 150);
+    pop();
+
+    imageMode(CENTER);
+    image(myImageLogo, width / 2, height / 2, diam, diam);
+
+    theta += .01;
 
 
   }
-}
 
-// function preload() {
-// buttonSound = loadSound("./assets/sounds/Sound_effect_1.mp3");
-// }
-//
-// function setup() {
-//   createCanvas(windowWidth, windowHeight);
-//   background(9, 14, 36);
-//
-//
-//   //enter button
-//   push();
-//   buttonEnter = createButton("ENTER THE GALAXY");
-//   buttonEnter.position(windowWidth/2-buttonEnter.width/2, windowHeight*8/9-buttonEnter.height/2);
-//   buttonEnter.mousePressed(changeSketch);
-//   pop();
-// }
-//
-//
-//
-// function draw() {
-//   // background("#090e24");
-//   background(9, 14, 36, 1);
-//
-//   // stelle random + stelle cadenti
-//   if (random() > 0.92) {
-//     stroke(255);
-//     ellipse(random(width), random(height), random(0.1, 1.5));
-//   }
-//
-//   if (random() > 0.95 && step >= 2.5) {
-//     fromX = random(width);
-//     fromY = random(height / 2);
-//     toX = random(fromX + 10, -width);
-//     toY = random(fromY + 10, -height / 2);
-//     step = 0;
-//   }
-//
-//   if (step < 2.5) {
-//     let nextStep = step + 0.02;
-//     strokeWeight(3);
-//     stroke(9, 14, 36, 100);
-//     line(fromY, fromX, toY, toX);
-//     strokeWeight(0.7);
-//
-//     if (step < 1) {
-//       stroke(255, (1 - step) * 200);
-//       line(lerp(fromY, toY, step), lerp(fromX, toX, step),
-//         lerp(fromY, toY, nextStep), lerp(fromX, toX, nextStep));
-//     }
-//     step = nextStep;
-//   }
-//
-//   push();
-//   fill(9, 14, 36);
-//   noStroke();
-//   rectMode(CENTER);
-//   rect(windowWidth / 2, windowHeight / 2.1, 250, 50);
-//   pop();
-//
-//   // testo Titolo
-//   push();
-//   fill("#dddddb");
-//   noStroke();
-//   textSize(40);
-//   textAlign(CENTER);
-//   textFont("Prata");
-//   text(myTitle, windowWidth / 2, windowHeight / 2);
-//   pop();
-//
-//   push();
-//   noStroke();
-//   fill(9, 14, 36);
-//   rectMode(CENTER);
-//   rect(windowWidth/2, windowHeight*8/9, 180, 40);
-//   pop();
-//
-// }
-//
-// function changeSketch() {
-//   window.open('concept.html', '_self');
-// }
-//
-//
-// //   function playSound() {
-// //
-// //   // If the user clicks on the button, play the sound!
-// // if(buttonSound.isPlaying() == false){
-// //
-// // buttonSound.play();
-// // }   window.open('concept.html', '_self');
-// //   }
-//
-//
-// function windowResized() {
-//   resizeCanvas(windowWidth, windowHeight);
-// }
+
+  function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+  }
+
+}
